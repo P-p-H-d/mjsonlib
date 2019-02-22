919ae46fc22a99125ac84790679a8d098c472fd3
Patrick Pelissier <patrick.pelissier@gmail.com>
2019-02-22 18:09:28 +0100

    Update test suite to read arbitrary JSON file.

--- test.c
+++ test.c
@@ -1,10 +1,36 @@
 #include <assert.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "mjsonlib.h"
 
-int main(void)
+static void test_file(const char filename[], bool printit)
+{
+  FILE *f = fopen(filename, "r");
+  if (f== NULL) {
+    fprintf(stderr, "ERROR: Cannot open %s\n", filename);
+    exit(1);
+  }
+  
+  mjson_t p;
+  bool b = json_init_in_str(&p, f);
+  if (!b) {
+    fprintf(stderr, "ERROR: Cannot parse JSON file in %s\n", filename);
+    exit(2);
+  }
+
+  if (printit) {
+    printf("JSON= ");
+    json_out_str(stdout, p);
+    printf("\n");
+  }
+
+  json_clear(p);
+  fclose(f);
+}
+
+static void test_suite(void)
 {
   FILE *f = fopen("test.json", "r");
   assert (f != NULL);
@@ -25,5 +51,13 @@ int main(void)
   const char *str = json_get_string(p, "c", "[3]", NULL);
   assert (strcmp(str, "Hello") == 0);
   json_clear(p);
-  return 0;
+}
+
+int main(int argc, const char *argv[])
+{
+  if (argc == 1)
+    test_suite();
+  else
+    test_file(argv[1], argc > 2);
+  exit(0);
 }
